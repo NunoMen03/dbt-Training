e3751ead652d8d970d7f365b87f6a15b2dfd4152
Nuno Mendes <nuno-miguel.mendes@capgemini.com>
2024-04-11 10:44:35 +0000

    Developing report

---
+++ models/reporting/report_profit_by_product.sql
@@ -0,0 +1,12 @@
+select
+    productid,
+    prodructname,
+    category,
+    subcategory,
+    sum(orderprofit) as profit
+from {{ ref('stg_orders') }}
+group by 
+    productid,
+    prodructname,
+    category,
+    subcategory
