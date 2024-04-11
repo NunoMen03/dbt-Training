9d00e66234af17cc0fe055030c9b2b2a2581af0f
Nuno Mendes <nuno-miguel.mendes@capgemini.com>
2024-04-11 15:43:16 +0000

    markup

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -4,21 +4,25 @@ o.orderid,
 o.orderdate,
 o.shipdate,
 o.shipmode,
-o.ordersellingprice - o.ordercostprice as orderprofit,
+o.customerid,
+o.productid,
 o.ordercostprice,
 o.ordersellingprice,
 --from raw_customer
-c.customerid,
 c.customername, 
 c.segment,
 c.country,
 --from raw_product
-p.productid,
 p.category,
 p.prodructname,
-p.subcategory
+p.subcategory,
+ordersellingprice - ordercostprice as orderprofit,
+{{ markup('ordersellingprice', 'ordercostprice' ) }} as markup
 from {{ ref('raw_orders') }} as o
 left join {{ ref('raw_customer') }} as c
 on o.customerid = c.customerid
 left join {{ ref('raw_product') }} as p
 on o.productid = p.productid
+
+{{limit_data_in_dev('orderdate')}}
+
